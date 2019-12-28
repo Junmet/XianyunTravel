49820030ad581e4affa91df9c628ed26aae6c402
JUNMET <1611859097@qq.com>
2019-12-28 17:32:23 +0800

    Nuxt环境的搭建

---
+++ pages/tab.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <div>
+          <span v-for='(i,index) in ["NBA","Recreation","News"]' :key='index'>{{i}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style  lang="less" scoped>
+span{
+    margin-right: 10px
+}
+
+</style>
