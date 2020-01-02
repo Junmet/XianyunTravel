b9a53d4da8186ba2da7204b326c910bb13eb189f
JUNMET <1611859097@qq.com>
2020-01-02 20:41:19 +0800

    实现了机票列表的分页功能

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -1,180 +1,191 @@
 <template>
-    <div class="flight-item">
-        <div>
-            <!-- 显示的机票信息 -->
-            <el-row type="flex" align="middle" class="flight-info">
-                <el-col :span="6">
-                    <span>{{data.airline_name}}</span> {{data.flight_no}}
-                </el-col>
-                <el-col :span="12">
-                    <el-row type="flex" justify="space-between" class="flight-info-center">
-                        <el-col :span="8" class="flight-airport">
-                            <strong>{{data.dep_time}}</strong>
-                            <span>{{data.org_airport_name}}{{data.org_airport_quay}}</span>
-                        </el-col>
-                        <el-col :span="8" class="flight-time">
-                            <!-- 时间差 -->
-                            <span>{{mistiming}}</span>
-                        </el-col>
-                        <el-col :span="8" class="flight-airport">
-                            <strong>{{data.arr_time}}</strong>
-                            <span>{{data.dst_airport_name}}{{data.dst_airport_quay}}</span>
-                        </el-col>
-                    </el-row>
-                </el-col>
-                <el-col :span="6" class="flight-info-right">
-                    ￥<span class="sell-price">{{data.base_price/2}}</span>起
-                </el-col>
-            </el-row>
-        </div>
-        <div class="flight-recommend" v-for="(item,index) in data.seat_infos" :key="index">
-            <!-- 隐藏的座位信息列表 -->
-            <el-row type="flex"  justify="space-between" align="middle">
-                <el-col :span="4">低价推荐</el-col>
-                <el-col :span="20">
-                    <el-row type="flex" justify="space-between" align="middle" class="flight-sell">
-                        <el-col :span="16" class="flight-sell-left">
-                            <span>{{item.name}}</span> | {{item.supplierName}}
-                        </el-col>
-                        <el-col :span="5" class="price">
-                            ￥{{item.par_price}}
-                        </el-col>
-                        <el-col :span="3" class="choose-button">
-                            <el-button 
-                            type="warning" 
-                            size="mini">
-                            选定
-                            </el-button>
-                            <p>剩余：{{item.discount}}</p>
-                        </el-col>
-                    </el-row>
-                </el-col>
-            </el-row>
-        </div>
+  <div class="flight-item">
+    <div @click="isShow = !isShow">
+      <!-- 显示的机票信息 -->
+      <el-row type="flex" align="middle" class="flight-info">
+        <el-col :span="6">
+          <span>{{data.airline_name}}</span>
+          {{data.flight_no}}
+        </el-col>
+        <el-col :span="12">
+          <el-row type="flex" justify="space-between" class="flight-info-center">
+            <el-col :span="8" class="flight-airport">
+              <strong>{{data.dep_time}}</strong>
+              <span>{{data.org_airport_name}}{{data.org_airport_quay}}</span>
+            </el-col>
+            <el-col :span="8" class="flight-time">
+              <!-- 时间差 -->
+              <span>{{mistiming}}</span>
+            </el-col>
+            <el-col :span="8" class="flight-airport">
+              <strong>{{data.arr_time}}</strong>
+              <span>{{data.dst_airport_name}}{{data.dst_airport_quay}}</span>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="6" class="flight-info-right">
+          ￥
+          <span class="sell-price">{{data.base_price/2}}</span>起
+        </el-col>
+      </el-row>
     </div>
+    <div class="flight-recommend" v-if="isShow">
+      <!-- 隐藏的座位信息列表 -->
+      <el-row
+        type="flex"
+        justify="space-between"
+        align="middle"
+        v-for="(item,index) in data.seat_infos"
+        :key="index"
+      >
+        <el-col :span="4">低价推荐</el-col>
+        <el-col :span="20">
+          <el-row type="flex" justify="space-between" align="middle" class="flight-sell">
+            <el-col :span="16" class="flight-sell-left">
+              <span>{{item.name}}</span>
+              | {{item.supplierName}}
+            </el-col>
+            <el-col :span="5" class="price">￥{{item.par_price}}</el-col>
+            <el-col :span="3" class="choose-button">
+              <el-button type="warning" size="mini">选定</el-button>
+              <p>剩余：{{item.discount}}</p>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    // props：有一种是数组的方式  一种是对象的方式（适合团队合作，知道传过来的数据是什么数据类型）
-    props: {
-        // 数据
-        data: {
-            // 数据类型是一个对象
-            type: Object,
-            // 要以函数的形式传一个对象
-            default(){return {}}
-        }
-    },
-    // 计算属性  实现时间差
-    computed: {
-        mistiming(){
-            // 切割字符串 返回一个数组
-           let arrtime = this.data.arr_time.split(":")
-           let deptime = this.data.dep_time.split(":")
-          
-            // 1小时为60分钟
-           let arr = (arrtime[0] * 60 + +arrtime[1])
-           let dep = (deptime[0] * 60 + +deptime[1])
-           
-           let res = arr - dep
-
-            //如果是第二天需要加24小时 要不然会是个负数
-           if(res < 0){
-            let res = arr + 24 * 60 - dep 
-           }
-           return `${Math.floor(res / 60)}时${res % 60}分`
-        }
+  data() {
+    return {
+      isShow: false
+    };
+  },
+  // props：有一种是数组的方式  一种是对象的方式（适合团队合作，知道传过来的数据是什么数据类型）
+  props: {
+    // 数据
+    data: {
+      // 数据类型是一个对象
+      type: Object,
+      // 要以函数的形式传一个对象
+      default() {
+        return {};
+      }
     }
-}
+  },
+  // 计算属性  实现时间差
+  computed: {
+    mistiming() {
+      // 切割字符串 返回一个数组
+      let arrtime = this.data.arr_time.split(":");
+      let deptime = this.data.dep_time.split(":");
+
+      // 1小时为60分钟
+      let arr = arrtime[0] * 60 + +arrtime[1];
+      let dep = deptime[0] * 60 + +deptime[1];
+
+      //如果是第二天需要加24小时 要不然会是个负数
+      if ((arr - dep) < 0) {
+        arr += 24 * 60;
+      }
+
+      let res = arr - dep;
+
+      return `${Math.floor(res / 60)}时${res % 60}分`;
+    }
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .flight-item{
-        border:1px #ddd solid;
-        margin-bottom: 10px;
+.flight-item {
+  border: 1px #ddd solid;
+  margin-bottom: 10px;
 
-        .flight-info{
-            padding:15px;
-            cursor: pointer;
+  .flight-info {
+    padding: 15px;
+    cursor: pointer;
 
-            > div{
-                &:first-child, &:last-child{
-                    text-align: center;
-                }
-            }
-        }
+    > div {
+      &:first-child,
+      &:last-child {
+        text-align: center;
+      }
+    }
+  }
 
-        .flight-info-center{
-            padding:0 30px;
-            text-align: center;
+  .flight-info-center {
+    padding: 0 30px;
+    text-align: center;
 
-            .flight-airport{
-                strong{
-                    display: block;
-                    font-size:24px;
-                    font-weight: normal;
-                }
-                span{
-                    font-size: 12px;
-                    color:#999;
-                }
-            }
-
-            .flight-time{
-                span{
-                    display: inline-block;
-                    padding:10px 0;
-                    border-bottom: 1px #eee solid;
-                    color:#999;
-                }
-            }
-        }
-
-        .flight-info-right{
-            
-            .sell-price{
-                font-size: 24px;
-                color:orange;
-                margin:0 2px;
-            }
-        }
+    .flight-airport {
+      strong {
+        display: block;
+        font-size: 24px;
+        font-weight: normal;
+      }
+      span {
+        font-size: 12px;
+        color: #999;
+      }
     }
 
-    .flight-recommend{
-        background: #f6f6f6;
-        border-top:1px #eee solid;
-        padding:0 20px;
-
-        .flight-sell{
-            border-bottom:1px #eee solid;
-            padding:10px 0;
-
-            &:last-child{
-                border-bottom: none;
-            }
-
-            .flight-sell-left{
-                font-size: 12px;
-                span{
-                    color:green;
-                }
-            } 
-
-            .price{
-                font-size: 20px;
-                color:orange;
-            }
-
-            .choose-button{
-                text-align: center;
-                color:#666;
-                button{
-                    display: block;
-                    width:100%;
-                    margin-bottom:5px;
-                }
-            }
-        }
+    .flight-time {
+      span {
+        display: inline-block;
+        padding: 10px 0;
+        border-bottom: 1px #eee solid;
+        color: #999;
+      }
     }
+  }
+
+  .flight-info-right {
+    .sell-price {
+      font-size: 24px;
+      color: orange;
+      margin: 0 2px;
+    }
+  }
+}
+
+.flight-recommend {
+  background: #f6f6f6;
+  border-top: 1px #eee solid;
+  padding: 0 20px;
+
+  .flight-sell {
+    border-bottom: 1px #eee solid;
+    padding: 10px 0;
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    .flight-sell-left {
+      font-size: 12px;
+      span {
+        color: green;
+      }
+    }
+
+    .price {
+      font-size: 20px;
+      color: orange;
+    }
+
+    .choose-button {
+      text-align: center;
+      color: #666;
+      button {
+        display: block;
+        width: 100%;
+        margin-bottom: 5px;
+      }
+    }
+  }
+}
 </style>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,76 +1,116 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                <FlightsListHead></FlightsListHead>
-                
-                
-                <!-- 航班信息 -->
-                <!-- 把获取到的数据传给子组件 :data="item"-->
-                <FlightsItem v-for="(item,index) in flightsData.flights" :key="index" :data="item"></FlightsItem>
+        <!-- 航班头部布局 -->
+        <FlightsListHead></FlightsListHead>
 
-            </div>
+        <!-- 航班信息 -->
+        <!-- 把获取到的数据传给子组件 :data="item"-->
+        <FlightsItem v-for="(item,index) in pagingList" :key="index" :data="item"></FlightsItem>
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <!-- 分页 -->
+        <!-- size-change：切换条数时候触发 -->
+        <!-- current-change：选择页数时候触发 -->
+        <!-- current-page: 当前页数 -->
+        <!-- page-size：当前条数 -->
+        <!-- total：总条数 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageIndex"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
 import moment from "moment";
 // 机票列表页头部组件的引入
-import FlightsListHead from "@/components/air/flightsListHead"
+import FlightsListHead from "@/components/air/flightsListHead";
 // 机票列表组件的引入
-import FlightsItem from "@/components/air/flightsItem"
+import FlightsItem from "@/components/air/flightsItem";
 export default {
-    data(){
-        return {
-            // 所有航班的总数据
-            flightsData:{}
-        }
-    },
-    // 注册
-    components:{
-        FlightsListHead,
-        FlightsItem
-    },
-    mounted(){
-        // console.log( this.$route); 拿到url地址栏的数据
-        this.$axios({
-            url:'/airs',
-            params:this.$route.query
-        }).then(res=>{
-            console.log(res);
-            this.flightsData = res.data
-        })
-    }
-}
+  data() {
+    return {
+      // 所有航班的总数据
+      flightsData: {},
+    //航班列表数据 用来实现分页
+      dataList:[],
+      pageIndex:1, //当前多少页
+      pageSize:5,  //多少条数据
+      total:0 //总页数
+    };
+  },
+  // 注册
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  mounted() {
+    // console.log( this.$route); 拿到url地址栏的数据
+    this.$axios({
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+      console.log(res);
+      // 所有航班的总数据
+      this.flightsData = res.data;
+      //航班列表数据 用来实现分页
+      this.dataList = this.flightsData.flights
+        // 获取总页数
+      this.total = this.flightsData.total
+    });
+  },
+//   使用计算属性 计算分页数据
+computed: {
+  pagingList(){
+    //   当没有数据的时候返回空数组
+      if(!this.dataList) return []
+
+    return  this.dataList.slice((this.pageIndex-1) * this.pageSize,this.pageIndex * this.pageSize)
+  }  
+},
+  methods:{
+      handleSizeChange(value){
+        //   console.log(value);
+          this.pageSize = value
+          
+      },
+      handleCurrentChange(value){
+        //   console.log(value);
+          this.pageIndex =  value
+      }
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
