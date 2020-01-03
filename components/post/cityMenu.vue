708f968bcd2606bbec6a2b67cb5711a6ca3c55d7
JUNMET <1611859097@qq.com>
2020-01-03 20:35:48 +0800

    旅游攻略页面左边栏完成

---
+++ components/post/cityMenu.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="contianer">
+    <div class="bor">
+      <div class="menu-item" @mouseenter="isShowMenu=true" @mouseleave="leave">
+        <span class="hov">热门城市</span>
+        <i class="el-icon-arrow-right ico"></i>
+      </div>
+      <div class="menu-item" @mouseenter="isShowMenu=true" @mouseleave="leave">
+        <span class="hov">热门城市</span>
+        <i class="el-icon-arrow-right ico"></i>
+      </div>
+      <div class="menu-item" @mouseenter="isShowMenu=true" @mouseleave="leave">
+        <span class="hov">热门城市</span>
+        <i class="el-icon-arrow-right ico"></i>
+      </div>
+      <div class="menu-item" @mouseenter="isShowMenu=true" @mouseleave="leave">
+        <span class="hov">热门城市</span>
+        <i class="el-icon-arrow-right ico"></i>
+      </div>
+      <div class="menu-item" @mouseenter="isShowMenu=true" @mouseleave="leave">
+        <span class="hov">热门城市</span>
+        <i class="el-icon-arrow-right ico"></i>
+      </div>
+    </div>
+    <div class="city-menu" v-if="isShowMenu">
+      <ul>
+        <li class="sitelist">
+          <span class="italic">1</span>
+          <span class="site">
+            <el-link class="link" :underline="false">
+              <strong class="city">北京</strong>
+              <span class="desc">实际的身份就拉开建设的发骄傲士大夫</span>
+            </el-link>
+          </span>
+        </li>
+        <li class="sitelist">
+          <span class="italic">1</span>
+          <span class="site">
+            <el-link class="link" :underline="false">
+              <strong class="city">北京</strong>
+              <span class="desc">实际的身份就拉开建设的发骄傲士大夫</span>
+            </el-link>
+          </span>
+        </li>
+        <li class="sitelist">
+          <span class="italic">1</span>
+          <span class="site">
+            <el-link class="link" :underline="false">
+              <strong class="city">北京</strong>
+              <span class="desc">实际的身份就拉开建设的发骄傲士大夫</span>
+            </el-link>
+          </span>
+        </li>
+        <li class="sitelist">
+          <span class="italic">1</span>
+          <span class="site">
+            <el-link class="link" :underline="false">
+              <strong class="city">北京</strong>
+              <span class="desc">实际的身份就拉开建设的发骄傲士大夫</span>
+            </el-link>
+          </span>
+        </li>
+        <li class="sitelist">
+          <span class="italic">1</span>
+          <span class="site">
+            <el-link class="link" :underline="false">
+              <strong class="city">北京</strong>
+              <span class="desc">实际的身份就拉开建设的发骄傲士大夫</span>
+            </el-link>
+          </span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //  @mouseleave="isShowMenu=false"
+      isShowMenu: false
+    };
+  },
+  methods: {
+    leave() {
+      this.isShowMenu = !this.isShowMenu;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.contianer {
+  position: relative;
+}
+.bor {
+  border-top: 1px solid #ddd;
+  border-left: 1px solid #ddd;
+}
+.menu-item {
+  position: relative;
+  height: 40px;
+  line-height: 40px;
+  border-bottom: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+  padding: 0 20px;
+  font-size: 14px;
+  z-index: 2;
+  &:hover {
+    color: orange;
+  }
+  .ico {
+    position: absolute;
+    right: 0;
+    top: 12px;
+    font-size: 20px;
+    color: #999;
+  }
+}
+.city-menu {
+  width: 350px;
+  height: 200px;
+  left: 259px;
+  top: 0px;
+  background-color: #fff;
+  position: absolute;
+  border-top: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  border-left: 1px solid #ddd;
+  .sitelist {
+    // line-height: 40px;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+  }
+  .italic {
+    display: inline-block;
+    color: orange;
+    font-style: italic;
+    font-weight: 400;
+    padding: 5px 3px 3px 14px;
+    font-size: 24px;
+    .site {
+      display: block;
+      margin-left: 130px;
+    }
+  }
+  .link {
+    color: orange;
+    .city {
+      padding-left: 15px;
+      font-size: 14px;
+      font-weight: 400;
+    }
+    .desc {
+      padding-left: 15px;
+      font-size: 14px;
+      //   font-weight: 400;
+      color: #999;
+    }
+  }
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,19 +1,74 @@
 <template>
-  <div>
-    <el-row>
+  <div class="container">
+    <el-row type="flex" justify="space-between" class="map">
+      <!-- 左侧边栏 -->
       <el-col :span="7">
-        
+        <div class="sidebar">
+          <!-- 城市列表 -->
+          <!-- <div class="citylist">123</div> -->
+          <CityMenu></CityMenu>
+
+          <!-- 推荐城市 -->
+          <div class="recommend">
+            <h4 class="aside-title">推荐城市</h4>
+            <a href="/post#" class="a_img">
+              <img class="recommendCity-img" src="http://157.122.54.189:9093/images/pic_sea.jpeg">
+            </a>
+          </div>
+        </div>
       </el-col>
       <el-col :span="17">
-        
+        <div class="post-wrapper"></div>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-export default {};
+import CityMenu from "@/components/post/cityMenu"
+export default {
+  components:{
+    CityMenu
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+}
+.map{
+margin-top: 30px
+}
+.sidebar{
+  width: 260px;
+  // position: relative;
+  // .citylist{
+  //   width: 260px;
+  //   height: 165px;
+  //   background-color: aqua;
+  // }
+  .recommend{
+    margin-top: 20px;
+    .aside-title{
+      font-weight: 400;
+      padding-bottom: 10px;
+      border-bottom: 1px solid #ddd;
+      margin-bottom: 10px;
+    }
+    .a_img{
+      display:inline-block;
+      width: 260px;
+      .recommendCity-img{
+        width:100%;
+      }
+    }
+  }
+}
+.post-wrapper{
+  width: 700px;
+  height: 500px;
+  background-color: red
+}
 </style>
