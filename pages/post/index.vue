138e2084b197466fe404c2b42170b1e168439e78
JUNMET <1611859097@qq.com>
2020-01-03 15:04:03 +0800

    旅游攻略页码布局

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-      旅游首页
+    <el-row>
+      <el-col :span="7">
+        
+      </el-col>
+      <el-col :span="17">
+        
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
