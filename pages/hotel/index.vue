a7bab1567754122d3e3327bd8c71ae580ce9224f
JUNMET <1611859097@qq.com>
2020-01-09 18:33:40 +0800

    酒店搜索页面布局完成

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -10,13 +10,22 @@
         <span class="el-breadcrumb__item">南京市酒店预订</span>
       </div>
     </div>
-    <el-form :model="numberValidateForm" ref="numberValidateForm" label-width="100px" class="demo-ruleForm">
-    <el-autocomplete placeholder="请输入目的地"></el-autocomplete>
-     <el-date-picker type="daterange" start-placeholder="开始日期" end-placeholder="结束日期">
-    </el-date-picker>
-      <el-form-item>
-        <!-- <el-button type="primary">提交</el-button> -->
-      </el-form-item>
+    <!-- 酒店搜索 -->
+    <el-form ref="numberValidateForm" class="form">
+      <el-row type="flex" :gutter="10">
+        <el-col>
+          <el-input placeholder="请输入内容"></el-input>
+        </el-col>
+        <el-col>
+          <el-date-picker type="daterange" start-placeholder="开始日期" end-placeholder="结束日期"></el-date-picker>
+        </el-col>
+        <el-col>
+          <el-input placeholder="人数未定" suffix-icon="el-icon-user" @focus="isShowSelect=true"></el-input>
+        </el-col>
+        <el-col>
+          <el-button type="primary">查看价格</el-button>
+        </el-col>
+      </el-row>
     </el-form>
   </div>
 </template>
@@ -24,9 +33,7 @@
 <script>
 export default {
   data() {
-    return {
-      numberValidateForm: {}
-    };
+    return {};
   }
 };
 </script>
@@ -35,6 +42,9 @@ export default {
 .container {
   width: 1000px;
   margin: 0 auto;
+  .form {
+    margin-bottom: 20px;
+  }
   .hotel {
     overflow: hidden;
     padding: 20px 0;
